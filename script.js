489ccd832dc41dcd8eea141f3aa92abc651cb930
Tequila1Boy <pushkin.rodion@yandex.ru>
2022-05-19 22:25:28 +0300

    da

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let oldX = 0;
 let oldY = 0;
 let oldZ = 0;
-// 85 / 90
 const watchValue = (oldValue,newValue) => {
     newValue = Math.floor(newValue)
     if(newValue - oldValue == 0){
@@ -14,19 +13,18 @@ const watchValue = (oldValue,newValue) => {
 }
 const accelerometerUpdate = (event) =>{
     let X = watchValue(oldX,event.accelerationIncludingGravity.x*10);
-    // let Y = watchValue(oldY,event.accelerationIncludingGravity.y*10);
-    // let Z = watchValue(oldZ,event.accelerationIncludingGravity.z*10);
-    // console.log(oldX,oldY,oldZ)
+    let Y = watchValue(oldY,event.accelerationIncludingGravity.y*10);
+    let Z = watchValue(oldZ,event.accelerationIncludingGravity.z*10);
     oldX = X;
-    // oldY = Y;
-    // oldZ = Z;
+    oldY = Y;
+    oldZ = Z;
     document.querySelector("#x").value = oldX;
-    // document.querySelector("#y").value = oldY;
-    // document.querySelector("#z").value = oldZ;
+    document.querySelector("#y").value = oldY;
+    document.querySelector("#z").value = oldZ;
     if (oldY < 0) {
         oldX = -oldX - 180;
     }
-    document.querySelector("#block").style.transform="rotate("+oldX+"deg)";
+    document.querySelector("#block").style.transform="rotate("+(-oldX)+"deg)";
 }
 
 if (window.DeviceMotionEvent == undefined) {
