8124889fc6a6869da1f9bee100452bfe606e228e
Tequila1Boy <pushkin.rodion@yandex.ru>
2022-05-19 19:51:38 +0300

    da

--- script.js
+++ script.js
@@ -9,21 +9,33 @@ else {
     document.querySelector("#acc").className = "yes";
     window.addEventListener("devicemotion", accelerometerUpdate, true);
 }
+const watchValue(oldValue,newValue){
+    if(newValue + 5 > oldValue){
+        return Math.floor(newValue)
+    }else if(newValue - 5 < oldValue){
+        return Math.floor(newValue)
+    }
+}
 
 
 function accelerometerUpdate(event) {
-    var aX = event.accelerationIncludingGravity.x*10;
-    var aY = event.accelerationIncludingGravity.y*10;
-    var aZ = event.accelerationIncludingGravity.z*10;
+    let X = watchValue(oldX,event.accelerationIncludingGravity.x*10);
+    let Y = watchValue(oldY,event.accelerationIncludingGravity.y*10);
+    let Z = watchValue(oldZ,event.accelerationIncludingGravity.z*10);
+    let oldX = 0;
+    let oldY = 0;
+    let oldZ = 0;
 
-    document.querySelector("#x").value = aX;
-    document.querySelector("#y").value = aY;
-    document.querySelector("#z").value = aZ;
+
+
+    document.querySelector("#x").value = oldX;
+    document.querySelector("#y").value = oldY;
+    document.querySelector("#z").value = oldZ;
 
     // ix aY is negative, switch rotation
-    if (aY <0) {
-        aX = -aX - 180;
+    if (oldY < 0) {
+        oldX = -oldX - 180;
     }
-    document.querySelector("#block").style.transform="rotate("+aX+"deg)";
+    document.querySelector("#block").style.transform="rotate("+oldX+"deg)";
 
 }
